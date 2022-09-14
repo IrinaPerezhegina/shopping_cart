9386006541a14dfa2ebf896061601fedbe652461
IrinaPerezhegina <perezeginaiv@mail.ru>
2022-09-14 14:12:02 +0500

    added project

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import CountersList from "./component/countersList";
+
+const App = () => {
+  return <CountersList />;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+import ReactDOM  from "react-dom";
+import 'bootstrap/dist/css/bootstrap.css'
+import App from "./app.jsx"
+
+ReactDOM.render(<App/>, document.getElementById('root'));
